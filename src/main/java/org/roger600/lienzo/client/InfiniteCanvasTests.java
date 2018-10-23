2d72cdd315e5d2ebc88a76588968b406395239dc
Roger Martinez <roger600@gmail.com>
2018-10-23 02:49:33 +0200

    Infinite Canvas integ

--- src/main/java/org/roger600/lienzo/client/InfiniteCanvasTests.java
+++ src/main/java/org/roger600/lienzo/client/InfiniteCanvasTests.java
@@ -2,23 +2,26 @@ package org.roger600.lienzo.client;
 
 import com.ait.lienzo.client.core.shape.Layer;
 import com.ait.lienzo.client.core.shape.Rectangle;
-import com.ait.lienzo.client.widget.LienzoPanel;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.dom.client.Style;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.RootPanel;
+import org.roger600.lienzo.client.widget.InfiniteLienzoLayer;
+import org.roger600.lienzo.client.widget.InfiniteLienzoPanel;
 
 public class InfiniteCanvasTests implements EntryPoint
 {
-    private LienzoPanel panel1 = new LienzoPanel(600, 600);
+    // private LienzoPanel panel = new LienzoPanel(600, 600);
+    private InfiniteLienzoPanel panel = new InfiniteLienzoPanel(600, 600);
 
-    private Layer       layer1 = new Layer();
+    // private Layer       layer = new Layer();
+    private InfiniteLienzoLayer layer = new InfiniteLienzoLayer();
 
     public void onModuleLoad()
     {
         drawLayout();
         drawThings();
-        layer1.draw();
+        getLayer().draw();
     }
 
     private void drawLayout()
@@ -26,13 +29,13 @@ public class InfiniteCanvasTests implements EntryPoint
         final HorizontalPanel v = new HorizontalPanel();
         v.getElement().getStyle().setBorderStyle(Style.BorderStyle.SOLID);
         v.getElement().getStyle().setBorderColor("#000000");
-        v.getElement().getStyle().setBorderWidth(5, Style.Unit.PX);
+        v.getElement().getStyle().setBorderWidth(1, Style.Unit.PX);
 
         RootPanel.get().add(v);
-        v.add(panel1);
+        v.add(panel);
 
-        layer1.setTransformable(true);
-        panel1.add(layer1);
+        // getLayer().setTransformable(true);
+        panel.add(layer);
     }
 
     private void drawThings()
@@ -40,11 +43,20 @@ public class InfiniteCanvasTests implements EntryPoint
 
         final Rectangle r1 = new Rectangle(50, 50);
         r1.setX(50).setY(50);
+        r1.setFillColor("#FF0000");
+        r1.setDraggable(true);
 
         final Rectangle r2 = new Rectangle(100, 100);
         r2.setX(150).setY(150);
+        r2.setFillColor("#0000FF");
+        r2.setDraggable(true);
 
-        layer1.add(r1);
-        layer1.add(r2);
+        getLayer().add(r1);
+        getLayer().add(r2);
+    }
+
+    private Layer getLayer()
+    {
+        return layer;
     }
 }
